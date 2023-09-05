3eac7750dca5c55bd1edbb186f75f04ee26c378d
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-09-05 07:42:24 +0600

    Mongodb is connected

--- index.js
+++ index.js
@@ -15,3 +15,27 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("Server is running on port ", port);
 });
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.iidrxjp.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  // Connect the client to the server	(optional starting in v4.7)
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+}
+
+run().catch(console.dir);
